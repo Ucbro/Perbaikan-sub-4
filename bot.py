d12693922c9d719e1685de2a1f234bce59fbaa10
Ucbroo <108119977+Ucbro@users.noreply.github.com>
2025-02-24 06:40:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,6 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 from datetime import datetime
 
-app = Client("my_bot")
-
-@app.on_signal(signal.SIGTERM)
-async def signal_handler(_, __):
-    print("Signal SIGTERM detected, but bot will continue running.")
-
 from config import (
     API_HASH,
     APP_ID,
@@ -28,6 +22,11 @@ from config import (
     TG_BOT_TOKEN,
     TG_BOT_WORKERS,
     PORT,
+app = Client("my_bot")
+
+@app.on_signal(signal.SIGTERM)
+async def signal_handler(_, __):
+    print("Signal SIGTERM detected, but bot will continue running.")
 )
 
 
