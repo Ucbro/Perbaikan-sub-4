4a335aee8e8c1fe993e5b431b260cd946f237e4f
Ucbroo <108119977+Ucbro@users.noreply.github.com>
2025-02-24 07:24:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,6 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 from datetime import datetime
 
-Bot = Client("my_bot")
-
-@Bot.on_signal(signal.SIGTERM)
-async def signal_handler(_, __):
-    print("Signal SIGTERM detected, shutting down bot...")
-    await Bot.stop()
-
-Bot.run()
-
 from config import (
     API_HASH,
     APP_ID,
